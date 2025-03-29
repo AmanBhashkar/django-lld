f4a2c9fdae105af8fc19aa33e060411bba76e051
Aman Bhashkar <a.bhashkar906@gmail.com>
2025-03-29 03:35:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from project-v0!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ store_products/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+import httpx
+
+
+# Create your tests here.
+class HelloWorldTestCase(TestCase):
+    def test_hello_world(self):
+        response = self.client.get('/api/hello/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), 'Hello World')
